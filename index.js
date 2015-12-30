411f721f0d0614a227803502d57d69e18b2df495
jianyulv <andy.lv@intel.com>
2015-12-30 10:51:06 +0800

    add missing code.

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function convertVar(nameHash, prefix, v) {
   var name = prefix + (Object.keys(nameHash).length + gulpObfuscate.seed);
 
   nameHash[v] = name;
-  if (gulpObfuscate.debug == true) {
+  if (gulpObfuscate.debug) {
     gutil.log('gulp-obfuscate (converting): ' + name + ' <-> ' + v );
   }
   return name;
-};
+}
 
 var gulpObfuscate = function(options) {
   if (options == void 0) { options = {}; }
